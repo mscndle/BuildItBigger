399e46976a5f2bcf19c41ee80f781b1f2618db65
Mandeep Condle <mandeepcondle@gmail.com>
2016-05-30 02:38:31 -0400

    cleanup and final mods

--- jokelib/src/main/java/com/example/JokeGen.java
+++ jokelib/src/main/java/com/example/JokeGen.java
@@ -18,7 +18,13 @@ public class JokeGen {
     }
 
     private void init() {
-        jokes = new String[] {"joke1", "joke2", "joke3", "joke4", "joke5"};
+        jokes = new String[] {
+                "What's the best thing about Switzerland?\nI don't know, but the flag is a big plus",
+                "I invented a new word!\nPlagiarism!",
+                "Helvetica and Times New Roman walk into a bar...\n\"Get out of here!\" shouts the bartender. \"We don't serve your type.",
+                "Knock! Knock!\nWho's there?\nControl Freak.\nCon...\nOkay, now you say, \"Control Freak who?\"",
+                "Hear about the new restaurant called Karma?\nThere’s no menu: You get what you deserve."
+        };
     }
 
     public String getJoke() {
